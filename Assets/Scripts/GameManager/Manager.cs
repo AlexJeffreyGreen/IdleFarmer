d165a6e7205539750948298001fda29aa381ae17
Alex J. Green <alexjeffreygreen@gmail.com>
2021-03-05 09:36:20 -0500

    Initial Implementation of Singleton for Game Management
    Singleton Manager for all operations that should execute / be available globally.

---
+++ Assets/Scripts/GameManager/Manager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.GameManager
+{
+    public sealed class Manager
+    {
+        private static readonly object _lock = new object();
+        private static Manager _manager;
+        public Manager() { }
+
+        
+        public static Manager GameManagement
+        {
+            get
+            {
+                lock(_lock)
+                {
+                    if(_manager == null)
+                        _manager = new Manager();
+                }
+                return _manager;
+
+            }
+        }
+
+
+        public void BuildOutTileGrid() { throw new NotImplementedException(); }
+        public SmartTiles.SmartTileBase FindTileByPosition() { throw new NotImplementedException(); }
+        public void DayManagement() { throw new NotImplementedException(); }
+
+    }
+}
